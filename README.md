8a8f8077d672c9e4721e24d00fb39da27729159f
Francisco <andrewplanet@gmail.com>
2017-08-19 09:36:01 -0400

    Updated with hotlinecss and more information load time

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Unauthorized design for digital newspaper La Cabilla
 
+### Information
+
+10 Requests, 197 B transferred, Finish 591 ms. DOMContentLoaded 636 ms, Load 1.04 s
+
 #### Important
 
 All assets belong to their respective owners.
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-let gulp  = require("gulp");
-let uncss = require("gulp-uncss");
+let gulp        = require("gulp");
+let uncss       = require("gulp-uncss");
 let browserSync = require("browser-sync");
-let htmlmin = require("gulp-htmlmin");
+let htmlmin     = require("gulp-htmlmin");
+let inline      = require("gulp-inline-source");
 
-let xvx = "node_modules/xvx/css/xvx.min.css";
+let hotline_in_node   = "node_modules/hotlinecss/css/hotline.min.css";
+let hotline_in_assets = "assets/stylesheets/hotline.css";
 
 gulp.task("server", function () {
 	browserSync({
@@ -13,20 +15,20 @@ gulp.task("server", function () {
 	})
 });
 
-gulp.task("css", function () {
-	return gulp.src(xvx)
+gulp.task("uncss", function () {
+	return gulp.src(hotline_in_node)
 		.pipe(uncss({
-			html: ['index.html']
+			html: ['./html/index.html']
 		}))
 		.pipe(gulp.dest("assets/stylesheets"))
 });
 
-gulp.task("min:html", function () {
-	return gulp.src('index.html')
+gulp.task("html", function () {
+	return gulp.src('./html/index.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task("default", ["server", "min:html"], function () {
-	gulp.watch("./index.html", ["css", "min:html", browserSync.reload]);
+gulp.task("default", ["server", "uncss", "html"], function () {
+	gulp.watch("./html/index.html", ["uncss", "html", browserSync.reload]);
 });
